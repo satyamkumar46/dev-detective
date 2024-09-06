13d5b885c4e054308bae7f13859b55112c17b143
Satyam kumar <144627386+satyamkumar46@users.noreply.github.com>
2024-09-06 22:29:12 +0530

    Update script.js

--- Dev Detective/script.js
+++ Dev Detective/script.js
@@ -153,7 +153,6 @@ function init() {
   //darMode = false -> light mode enable karna h 
   darkMode = false;
 
-  //HW
 // const prefersDarkMode = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
 
   const value = localStorage.getItem("dark-mode");
